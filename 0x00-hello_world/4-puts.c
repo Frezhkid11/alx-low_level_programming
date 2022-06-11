e34a5772dd4fe9577e04409d2fb005f280e88154
Frezhkid11 <Frezhkid11@gmail.com>
2022-06-11 12:04:06 -0700

    Error corrected in first c script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-/**Writing a string to standard output */
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
