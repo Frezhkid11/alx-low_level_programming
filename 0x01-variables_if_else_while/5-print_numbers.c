a5212c8d2f2b4f2c2b92457fa68332a5ff4b4850
Frezhkid11 <Frezhkid11@gmail.com>
2022-06-14 07:31:38 -0700

    Printing singke digit decimals

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digits;
+
+	digits = 0;
+	/*Loop to restrict more than 10*/
+	while (digits < 10)
+	{
+		printf("%d\n", digits);
+		digits++;
+	}
+	return (0);
+}
+
