f006555a778ab2adda4020b06220afb49fc78389
Oyanaghakie Nosa <Frezhkid11@gmail.comuser.email frezhkid11@gmail.com>
2022-09-15 01:51:04 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * Main - prints _putchar
+ *
+ * Description - Prints  _putchar
+ *
+ * Return : 0 (Success)
+ */
+
+int main(void)
+{
+	char text[9] = "_putchar";
+
+	printf("%s\n", text);
+
+	return (0);
+}
