0f84883485fe9ddd20599a967e294677a997fbc3
Oyanaghakie Nosa <Frezhkid11@gmail.comuser.email frezhkid11@gmail.com>
2022-09-19 10:40:15 -0700

    print last digit of numbers

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,14 +8,15 @@
 int print_last_digit(int n)
 {
 	int a;
-	int r;
+
+	if (n < 0)
+		n = -n;
 
 	a = n % 10;
+
 	if (a < 0)
-	{
-		r = -1 * a;
-		return (r);
-	}
-	else
-		return (a);
+		a = -a;
+
+	_putchar(a + '0');
+	return (a);
 }
