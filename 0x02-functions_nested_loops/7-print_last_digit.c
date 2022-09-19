b43bc9f53fbe7166a852b2daef1eeb1067d281ad
Oyanaghakie Nosa <Frezhkid11@gmail.comuser.email frezhkid11@gmail.com>
2022-09-19 09:35:41 -0700

    Print the last digits of numbers

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - Print the last digit of an interger
+ * @n: interger to be passed as argument
+ * Return: The last digit of a number
+ */
+
+int print_last_digit(int n)
+{
+	int r;
+
+	r = n % 10;
+	return (r);
+}
