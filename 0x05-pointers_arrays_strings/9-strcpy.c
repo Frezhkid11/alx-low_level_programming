fe9db8d128e1bace0e84e8efa45ee31527b1766e
Oyanaghakie Nosa <Frezhkid11@gmail.comuser.email frezhkid11@gmail.com>
2022-09-20 16:05:09 -0700

    copy strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+include "main.h"
+/**
+ * *_strcpy - copy the contents of the latter string to the former
+ * @src: string to be copied from
+ * @dest: string to be copied to
+ * Return: pointer to string copied
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int j;
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[j] = src[j];
+	}
+	dest[j] = '\0';
+	return (dest);
+}
