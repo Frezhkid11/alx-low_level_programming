c5589e01907aa0c813f36982d9a991623f6d43ec
Oyanaghakie Nosa <Frezhkid11@gmail.comuser.email frezhkid11@gmail.com>
2022-09-20 01:36:22 -0700

    sums up two intergers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - sum the two intergers passed as argument
+ * @n: interger passed as argument
+ * @p: interger passed as argument
+ * Return: the sum of the two integers
+ */
+
+int add(int n, int p)
+{
+	int n, p;
+
+	return (n + p);
+}
