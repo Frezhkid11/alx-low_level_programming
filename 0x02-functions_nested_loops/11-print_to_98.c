f6dbe234d7234cbd18ebfccefc7483f894211658
Oyanaghakie Nosa <Frezhkid11@gmail.comuser.email frezhkid11@gmail.com>
2022-09-20 02:20:21 -0700

    Error corrected

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,17 +10,21 @@ void print_to_98(int n)
 	if (n <= 98)
 	{
 		for (n = n; n <= 97; n++)
+		{
 			_putchar(n);
 			_putchar(',');
 			_putchar(' ');
+		}
 	}
 	else
 	{
 		for  (n = n; n > 98; n--)
+		{
 			_putchar(n);
 			_putchar(',');
 			_putchar(' ');
+		}
 	}
-	_putchar("98");
+	_putchar('98');
 	_putchar('\n');
 }
