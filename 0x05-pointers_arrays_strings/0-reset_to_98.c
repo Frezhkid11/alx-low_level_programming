9b5eb1588057579574431078d2dbc5b3e292e8c2
Oyanaghakie Nosa <Frezhkid11@gmail.comuser.email frezhkid11@gmail.com>
2022-09-20 08:18:19 -0700

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - reset the valve of the variable to 98 using its pointer
+ * @*n: The pointer used
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
