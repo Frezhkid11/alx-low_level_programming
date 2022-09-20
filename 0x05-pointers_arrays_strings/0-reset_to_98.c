0c3f07abe0582732778c3b0a2e499f0e7444ae47
Oyanaghakie Nosa <Frezhkid11@gmail.comuser.email frezhkid11@gmail.com>
2022-09-20 08:27:28 -0700

    error

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * reset_to_98 - reset the valve of the variable to 98 using its pointer
- * @n: The pointer used
- * Return: nothing
+ * @n: pointer used
+ * Return: void
  */
 
 void reset_to_98(int *n)
