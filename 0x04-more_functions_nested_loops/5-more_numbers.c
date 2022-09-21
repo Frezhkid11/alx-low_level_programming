3394f6e73700bd84e78f349ee8c3f3972b26a43e
Oyanaghakie Nosa <Frezhkid11@gmail.comuser.email frezhkid11@gmail.com>
2022-09-21 01:50:42 -0700

    print 1-14 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers -a function that prints 1-14 10 times
+ *
+ * Return: Void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 15)
+		{
+			if (j >= 10)
+				_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+			j++;
+		}
+		_putchar('\n');
+		i++
+	}
+}
