6cd7ebace7856509e3b395f7c68754546f46678f
Oyanaghakie Nosa <Frezhkid11@gmail.comuser.email frezhkid11@gmail.com>
2022-09-21 08:42:02 -0700

    Changed output to string

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,15 +15,15 @@ int main(void)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("Fizzbuzz");
+			printf("%s", "Fizzbuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz");
+			printf("%s", "Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz");
+			printf("%s", "Buzz");
 		}
 		else
 		{
