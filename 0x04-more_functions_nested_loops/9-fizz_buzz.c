8bd3ec8dffe30993d546fc28f993f23b2e682bb4
Oyanaghakie Nosa <Frezhkid11@gmail.comuser.email frezhkid11@gmail.com>
2022-09-21 08:33:38 -0700

    Print fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("Fizzbuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
