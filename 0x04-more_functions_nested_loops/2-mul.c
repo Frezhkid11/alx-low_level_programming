7210aac1ff53078df0500b2ab4cb3e4c3550287d
Oyanaghakie Nosa <Frezhkid11@gmail.comuser.email frezhkid11@gmail.com>
2022-09-20 17:14:18 -0700

    multiplies two intergers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies two intergers
+ * @a: argument to the function
+ * @b: argument to the unction
+ * Return: the product of the two argument
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
