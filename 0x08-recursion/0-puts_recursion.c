d24c515a5aa7e575c3070800afb31fbe0d499f5c
Oyanaghakie Nosa <Frezhkid11@gmail.comuser.email frezhkid11@gmail.com>
2022-10-02 00:53:35 -0700

    error corrected

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * _puts_recursion - using recursion to print a string
- * @s: the s passed as argument
+ * @*s: the s passed as argument
  * Return: 0 successful
  */
 
 void _puts_recursion(char *s)
 {
-	if (s != '\0')
+	if (*s != '\0')
 	{
-		_putchar(s);
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
 	else
