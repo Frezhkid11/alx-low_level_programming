c82d6c7375b65576ff40def384140397e755f00e
Oyanaghakie Nosa <Frezhkid11@gmail.comuser.email frezhkid11@gmail.com>
2022-10-10 16:35:55 -0700

    whatsmy name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: counter of argument
+ * @argv[]: Array containing strings passed as argument
+ * Return: Always (0) Successful
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	i = 0;
+	printf("%s", argv[i]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is a directory containing all the project of argc & argv accoding to alx.
