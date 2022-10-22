7be79cd9c98eb15c0dd8069de8196de1c33ca52d
Oyanaghakie Nosa <Frezhkid11@gmail.comuser.email frezhkid11@gmail.com>
2022-10-22 08:24:58 -0700

    fixed the return pointer

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,17 +6,17 @@
  * Return: a pointer if found or null
  */
 
-char *_strpbrk(char *s, char *accept);
+char *_strpbrk(char *s, char *accept)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] == accept[j])
+			if (s[i] == *(accept + j))
 			{
-				return (s);
+				return (&s[i]);
 			}
 		}
 	}
