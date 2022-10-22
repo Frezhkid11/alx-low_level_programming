74ff81590da88a866a2089579d671d85639912fb
Oyanaghakie Nosa <Frezhkid11@gmail.comuser.email frezhkid11@gmail.com>
2022-10-22 05:48:38 -0700

    substring search

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: the string to be searched
+ * @accept: the string containing the characters to evaluate
+ * Return: the value of charaters matched
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, f, flag;
+
+	f = 0;
+	for (i = 0; s[i] != '\0'; j++)
+	{
+		flag = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				f++;
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+		{
+			return (f);
+		}
+	}
+	return (0);
+}
